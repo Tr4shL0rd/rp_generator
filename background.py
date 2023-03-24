5b45f982ba211745069bcf8efe4768ed22f504d2
Tr4shL0rd <ketil04@gmail.com>
2023-03-24 11:45:02 +0100

    typo fixes and fine tuning

--- background.py
+++ background.py
@@ -24,8 +24,8 @@ BASE_KEYWORDS = [
                 "Backstory",
                 "story",
                 "DND",
-                "dungeons and dragons",
-                "dungeons & dragons"
+                "\"dungeons and dragons\"",
+                "\"dungeons & dragons\""
                 ]
 
 
@@ -34,10 +34,10 @@ def get_keywords(character:Character):
     appends the generated character paramentes to the keywords
     """
     return BASE_KEYWORDS + [character.Presenting_gender,
-                            character.Race_description,
+                            #character.Race_description,
                             character.Class,
                             character.Race,
-                            character.Role,
+                            #character.Role,
                             f"{character.Spec} {character.Class}"]
 
 def create_backstory(character:Character):
@@ -45,24 +45,29 @@ def create_backstory(character:Character):
     creates a backstory for the character based on keywords
     """
     keywords = get_keywords(character)
-    DEBUG(keywords)
-    prompt = f"use these keywords for the backstory: {keywords}. "\
+    #DEBUG(keywords)
+    keywords_prompt = ", ".join(keywords)
+    prompt = f"use these keywords for the backstory: {keywords_prompt}. "\
             "the backstory has to be written for the perspectiv of the character. "\
             "The backstory cannot be longer than 255 symbols (letters, spaces, etc...). "\
             "Words such as \"DPS\", \"Tank\" and \"Healer\" refers to the characters role. "\
                 "so if DPS, the character is a damage dealer, and so forth. "\
+                "so dont say that the character is a DPS, instead say they are " \
+                    "\"adept at fighting\" or if its a tank, "\
+                    "then they're \"good at protection people\". "\
             "Avoid saying the name \"World of Warcraft\". "\
             "Please note that the world the character lives in is called \"Azeroth\". "\
             "Finish the backstory with the \"END BACKSTORY\". "\
             "create a short character backstory. "\
             "the charaters short description is: "\
-            f"i'm a {character.Presenting_gender} {character.Race_description} {character.Class} "
+            f"I'm {character.Name},"\
+                f" a {character.Presenting_gender} {character.Race_description} {character.Class} "
     resp = openai.Completion.create(
         model=models["davinci-003"],
         prompt=prompt,
         max_tokens=256,
         stop=["END BACKSTORY"],
-        n=1, # generates 5 completions
+        n=1, # generates n backstories
         temperature=0.7,
     )
     return resp.choices[0].text
